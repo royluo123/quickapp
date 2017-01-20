9abcacfd3b1dd44cc2f4cde6bd003f338f55aefc
royluo123 <royluo123@gmail.com>
2017-01-20 17:03:40 +0800

    Add project

---
+++ app/src/main/java/com/roy/quickapp/quickapp/QuickActivity.java
@@ -0,0 +1,13 @@
+package com.roy.quickapp.quickapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuickActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quick);
+    }
+}
